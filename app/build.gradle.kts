bffda788e262034f186f6edf325defd6aff43ff4
刘鑫 <liuxin@example.com>
2026-04-01 00:15:11 +0800

    chore: v0.3.0 版本号更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.benben.bendown_android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
